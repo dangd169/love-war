ef24e63cbd23c80e422e18ea18d4485b5e163819
Danny <shortieofcheese@gmail.com>
2015-11-14 16:28:35 -0500

    Color Calibration/ Hover Interaction

---
+++ love&war/Assets/Scripts/MoveCursor.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class MoveCursor : MonoBehaviour {
+
+
+
+	public float speed;
+	
+	void Update () {
+
+		if(gameObject.tag == "RED"){
+		Vector3 move = new Vector3(Input.GetAxis("Horizontal1"), Input.GetAxis("Vertical1"), 0);
+		transform.position += move * speed * Time.deltaTime;
+		}
+
+		if(gameObject.tag == "BLUE"){
+			Vector3 move = new Vector3(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"), 0);
+			transform.position += move * speed * Time.deltaTime;
+		}
+
+
+
+	}
+}
